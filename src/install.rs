a929fc6d80290b2700a39613d7562fa7eb3aaf98
Tomás Hernández <git@thernandez.me>
2026-05-01 00:57:25 -0400

    feat: display from/to version during activation

--- src/install.rs
+++ src/install.rs
@@ -37,11 +37,11 @@ pub fn install(version_str: &str) -> Result<()> {
         download_version(&url, &tag)?;
     }
 
-    println!(
-        "{} Activating Deno {}...",
-        style("◆").magenta(),
-        style(&tag).cyan().bold(),
-    );
+    let from = symlink::active_version();
+    match &from {
+        Some(f) => println!("{} Activating Deno {} \u{2192} {}...", style("◆").magenta(), style(f).cyan().bold(), style(&tag).cyan().bold()),
+        None => println!("{} Activating Deno {}...", style("◆").magenta(), style(&tag).cyan().bold()),
+    }
     symlink::activate(&tag)?;
     println!(
         "{} Installed Deno {} successfully.",
